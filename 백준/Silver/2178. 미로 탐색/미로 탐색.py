21685c9fca57aefc59c12d4695263d39b713f1a1
jiu <118248199+jiu6525@users.noreply.github.com>
2023-06-01 21:24:34 +0900

    [Silver I] Title: 미로 탐색, Time: 164 ms, Memory: 116428 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -0,0 +1,24 @@
+from collections import deque
+n,m = map(int,input().split())
+arr = []
+for i in range(n):
+    arr.append(list(map(int,input())))
+
+def bfs(a,b):
+    q = deque([(a,b)])
+    while q:
+        x,y = q.popleft()
+        for i,j in ((-1,0),(1,0),(0,-1),(0,1)):
+            dx = x + i
+            dy = y + j
+            if dx<0 or dy<0 or dx>=n or dy>=m:
+                continue
+            if arr[dx][dy] == 0 or arr[dx][dy] != 1:
+                continue
+            else:
+                arr[dx][dy] = arr[x][y] + 1
+                q.append([dx,dy])
+
+    return arr[-1][-1]
+
+print(bfs(0,0))
