6ec8af091940f98a05b4c5db20c44f4b52a2b565
jiu <118248199+jiu6525@users.noreply.github.com>
2023-07-16 22:06:06 +0900

    [level 0] Title: 암호 해독, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120892. 암호 해독/암호 해독.py
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.py
@@ -1,4 +1,2 @@
 def solution(cipher, code):
-    idx = code -1
-    answer = cipher[idx] + "".join([cipher[i] for i in range(len(cipher)) if (i-idx)%code == 0 and i>idx])
-    return answer
+    return cipher[code-1::code]
