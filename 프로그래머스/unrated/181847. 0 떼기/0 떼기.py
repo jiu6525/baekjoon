1fde99b8901b9c65b805f68c6bcab6a8685db545
jiu <118248199+jiu6525@users.noreply.github.com>
2023-07-20 12:59:42 +0900

    [unrated] Title: 0 떼기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181847. 0 떼기/0 떼기.py
+++ 프로그래머스/unrated/181847. 0 떼기/0 떼기.py
@@ -1,8 +1,4 @@
 def solution(n_str):
-    answer = n_str
-    if n_str[0] == '0':
-        for i in range(len(n_str)):
-            if n_str[i] != "0":
-                answer = n_str[i:]
-                break
-    return answer
+    for i in range(len(n_str)):
+        if n_str[i] != "0":
+            return n_str[i:]
