514e1fce53cc482346199e1467ba9b838614dc76
jiu <118248199+jiu6525@users.noreply.github.com>
2023-08-13 16:44:12 +0900

    [unrated] Title: 날짜 비교하기, Time: 0.00 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.py
@@ -0,0 +1,7 @@
+def solution(date1, date2):
+    for i in range(len(date1)):
+        if date1[i] < date2[i]:
+            return 1
+        elif date1[i] > date2[i]:
+            return 0
+    return 0
