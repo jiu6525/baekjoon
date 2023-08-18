c2b8d4ee17d0cc4faaaed801186acc59d4e367a0
jiu <118248199+jiu6525@users.noreply.github.com>
2023-08-18 14:40:20 +0900

    [unrated] Title: 리스트 자르기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181897. 리스트 자르기/리스트 자르기.py
+++ 프로그래머스/unrated/181897. 리스트 자르기/리스트 자르기.py
@@ -1,11 +1,6 @@
 def solution(n, slicer, num_list):
-    a,b,c = slicer
-    if n == 1:
-        answer = num_list[:b+1]
-    elif n == 2:
-        answer = num_list[a:]
-    elif n == 3:
-        answer = num_list[a:b+1]
-    else:
-        answer = num_list[a:b+1:c]
-    return answer
+    a,b,c=slicer
+    if n==1: return num_list[:b+1]
+    if n==2: return num_list[a:]
+    if n==3: return num_list[a:b+1]
+    return num_list[a:b+1:c]
