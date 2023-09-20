637ea0a7da44f6ff05e5fcfb08667506a4e41fe4
jiu <118248199+jiu6525@users.noreply.github.com>
2023-09-20 16:19:00 +0900

    [level 2] Title: 행렬의 곱셈, Time: 5.00 ms, Memory: 28.7 MB -BaekjoonHub

--- 프로그래머스/lv2/12949. 행렬의 곱셈/행렬의 곱셈.py
+++ 프로그래머스/lv2/12949. 행렬의 곱셈/행렬의 곱셈.py
@@ -1,13 +1,4 @@
 import numpy as np
 
 def solution(arr1, arr2):
-
-    a = np.array(arr1)
-    b = np.array(arr2)
-    c = np.dot(a, b)
-    
-    answer = []
-    for i in c:
-        answer.append(list(map(int,i)))
-
-    return answer
+    return [[sum(a*b for a,b in zip(X_row,Y_col)) for Y_col in zip(*arr2)] for X_row in arr1]
