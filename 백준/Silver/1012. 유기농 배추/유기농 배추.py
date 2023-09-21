635ce3273beee03c6a34ad4b22a1f1bc19a89bab
jiu <118248199+jiu6525@users.noreply.github.com>
2023-09-21 14:56:58 +0900

    [Silver II] Title: 유기농 배추, Time: 168 ms, Memory: 116860 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,5 +1,5 @@
 def dfs(a,b):
-    if a<0 or b<0 or a>=50 or b>=50:
+    if a<0 or b<0 or a>=n or b>=m:
         return False
     if arr[a][b] == 1:
         arr[a][b] = 0 
@@ -16,15 +16,15 @@ t = int(input())
 answer = []
 for i0 in range(t):
     n,m,k = map(int,input().split())
-    arr = [[0 for i in range(50)] for j in range(50)]
+    arr = [[0 for _ in range(50)] for __ in range(50)]
 
     for i1 in range(k):
         x,y = map(int,input().split())
         arr[x][y] = 1
 
     result = 0
-    for i in range(50):
-        for j in range(50):
+    for i in range(n):
+        for j in range(m):
             if dfs(i,j):
                 result += 1
     answer.append(result)
