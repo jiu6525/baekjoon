d6329ae3b3f26d24ac6f1c98baa349012b1c047c
jiu <118248199+jiu6525@users.noreply.github.com>
2023-10-14 20:25:05 +0900

    [Silver II] Title: 안녕, Time: 224 ms, Memory: 112928 KB -BaekjoonHub

--- 백준/Silver/1535. 안녕/안녕.py
+++ 백준/Silver/1535. 안녕/안녕.py
@@ -1,27 +1,21 @@
-def dfs(v,t_happy,k_hp):
+def dfs(v,n_happy,n_hp):
     global max_happy
-    if k_hp>=hp:
+    if n_hp>= hp:
         return
-    if max_happy<t_happy:
-        max_happy = t_happy
-    if v == N:
+    if n_happy > max_happy:
+        max_happy = n_happy
+    if v == n:
         return
-    
-    happy, ihp = l[v]
+    ihappy, ihp = lst[v]
+    dfs(v+1,n_happy+ihappy,n_hp+ihp)
+    dfs(v+1,n_happy,n_hp)
 
-    dfs(v+1,t_happy+happy,k_hp+ihp)
-    dfs(v+1,t_happy,k_hp)
-    
-N = int(input())
+n = int(input())
 hp = 100
-l = []
-a = list(map(int,input().split()))
-b = list(map(int,input().split()))
-l = list(map(list,zip(b,a)))
-
+damage_l = list(map(int,input().split()))
+happy_l = list(map(int,input().split()))
+lst = list(map(list,zip(happy_l,damage_l)))
 max_happy = 0
+
 dfs(0,0,0)
-
 print(max_happy)
-
-
