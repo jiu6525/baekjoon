084f545c24aa67c070bdcca3522ce4357134ac5a
jiu <118248199+jiu6525@users.noreply.github.com>
2023-11-16 13:11:30 +0900

    [Gold III] Title: 줄 세우기, Time: 260 ms, Memory: 118576 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.py
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.py
@@ -1,10 +1,8 @@
 from collections import deque
-
 n,m = map(int,input().split())
 student = [[] for i in range(n+1)]
 degree = [0 for i in range(n+1)]
 que = deque()
-
 for i in range(m):
     x,y = map(int,input().split())
     student[x].append(y)
@@ -21,4 +19,3 @@ while que:
         degree[s] -= 1
         if degree[s] == 0:
             que.append(s)
-
