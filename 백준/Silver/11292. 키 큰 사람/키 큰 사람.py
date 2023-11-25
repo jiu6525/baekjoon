ece77466f7498c387aba84c41f93d83067b306f6
jiu <118248199+jiu6525@users.noreply.github.com>
2023-11-25 15:05:36 +0900

    [Silver V] Title: 키 큰 사람, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Silver/11292. 키 큰 사람/키 큰 사람.py
@@ -0,0 +1,16 @@
+while 1:
+    n = int(input())
+    if n == 0:
+        break
+
+    max_height = 0
+    lst = []
+    for i in range(n):
+        name, height = input().split()
+        height = float(height)
+        if height > max_height:
+            lst = [name]
+            max_height = height
+        elif height == max_height:
+            lst.append(name)
+    print(*lst)
