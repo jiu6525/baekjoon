2e09c89526b7c6d415025e2d8c65fc84cd81d841
jiu <118248199+jiu6525@users.noreply.github.com>
2023-11-27 18:21:10 +0900

    [Silver III] Title: 순열 사이클, Time: 356 ms, Memory: 141884 KB -BaekjoonHub

--- 백준/Silver/10451. 순열 사이클/순열 사이클.py
+++ 백준/Silver/10451. 순열 사이클/순열 사이클.py
@@ -1,28 +1,18 @@
-from collections import deque
+def dfs(v):
+    visited[v] = 1
+    node = lst[v]
+    if visited[node] == 0:
+        dfs(node)
+        return
+
 for i0 in range(int(input())):
-    queue = deque()
     n = int(input())
     a = [i for i in range(1,n+1)]
-    lst = list(map(int,input().split()))
-
-    arr = [[] for i in range(n+1)]
-
-    for i in range(n):
-        arr[a[i]].append(lst[i])
-        
-    ans = 0
+    lst = [0] + list(map(int,input().split()))
+    visited = [0] + [0 for i in range(1,n+1)]
+    cnt = 0
     for i in range(1,n+1):
-        if arr[i]:
-            q = arr[i].pop()
-            queue.append(q)
-            while queue:
-                node = queue.popleft()
-                if not arr[node]:
-                    break
-                else:
-                    for j in arr[node]:
-                        queue.append(j)
-                    arr[node].pop()
-            ans += 1
-
-    print(ans)
+        if visited[i] == 0:
+            dfs(i)
+            cnt += 1
+    print(cnt)
