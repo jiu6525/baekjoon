720aa8db3c892c287c6f990174b1291d0216d9aa
jiu <118248199+jiu6525@users.noreply.github.com>
2023-11-30 09:56:21 +0900

    [Silver II] Title: Oil Deposits, Time: 136 ms, Memory: 111916 KB -BaekjoonHub

---
+++ 백준/Silver/4677. Oil Deposits/Oil Deposits.py
@@ -0,0 +1,34 @@
+def dfs(x,y):
+    if x<0 or y<0 or x>=n or y>=m:
+        return 0
+    if lst[x][y] == "@":
+        lst[x][y] = "*"
+
+        dfs(x,y+1)
+        dfs(x,y-1)
+        dfs(x-1,y)
+        dfs(x+1,y)
+        dfs(x-1,y-1)
+        dfs(x-1,y+1)
+        dfs(x+1,y+1)
+        dfs(x+1,y-1)
+
+        return 1
+    return 0
+
+while 1:
+    n,m = map(int,input().split())
+    if n==0 and m==0:
+        break
+    lst = []
+    for i in range(n):
+        lst.append(list(input()))
+
+    result = 0
+    for i in range(n):
+        for j in range(m):
+            if lst[i][j] == "@":
+                if dfs(i,j):
+                    result += 1
+
+    print(result)
