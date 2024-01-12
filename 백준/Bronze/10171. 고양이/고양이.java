5fb229ccc74ea5fe0f97bcb3114e87edbd229647
jiu <118248199+jiu6525@users.noreply.github.com>
2024-01-12 16:35:45 +0900

    [Bronze V] Title: 고양이, Time: 124 ms, Memory: 13904 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+class Main{	
+	public static void main(String[] args) throws IOException{
+
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+		
+		
+		
+		
+		
+		}
+}
