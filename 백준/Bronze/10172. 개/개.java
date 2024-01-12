964d2d0d2f8a5325e6c909122f1279109e9312c2
jiu <118248199+jiu6525@users.noreply.github.com>
2024-01-12 16:36:35 +0900

    [Bronze V] Title: 개, Time: 112 ms, Memory: 13860 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+class Main{	
+	public static void main(String[] args) throws IOException{
+
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|");
+
+		}
+}
