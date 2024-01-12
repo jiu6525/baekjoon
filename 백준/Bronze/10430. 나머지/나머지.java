85123b918ba70a954ee79f300b62fa4ea88e0991
jiu <118248199+jiu6525@users.noreply.github.com>
2024-01-12 16:13:15 +0900

    [Bronze V] Title: 나머지, Time: 132 ms, Memory: 14408 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+class Main{	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+		
+
+		System.out.printf("%d %d %d %d",((a+b)%c),((a%c)+(b%c))%c,(a*b)%c,((a%c)*(b%c))%c);
+		}
+}
