629676cb7f7759758c00aad51dea5333167a6321
jiu <118248199+jiu6525@users.noreply.github.com>
2024-01-12 09:42:23 +0900

    [Bronze V] Title: A/B, Time: 120 ms, Memory: 14340 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main{	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		
+		int a = Integer.parseInt(st.nextToken());
+		double b = Integer.parseInt(st.nextToken());
+		
+//		System.out.printf("%f",a/b);
+		System.out.println(a/b);
+		br.close();
+		}
+}
