5c5597c9c8b434515d80d7ce265cb1245153ec03
jiu <118248199+jiu6525@users.noreply.github.com>
2024-01-23 11:51:56 +0900

    [Bronze II] Title: 영식이와 친구들, Time: 124 ms, Memory: 14284 KB -BaekjoonHub

---
+++ 백준/Bronze/1592. 영식이와 친구들/영식이와 친구들.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		int l = Integer.parseInt(st.nextToken());
+		int[] arr = new int[n];
+		
+		int num = 0;
+		int cnt = 0;
+		int idx = 0;
+		arr[0] = 1;
+		
+		while(num < m) {
+			cnt++;
+			num = arr[idx];
+			
+			if(arr[idx] % 2 != 0) {
+				idx = (idx + l)%n;
+			}else {
+				idx = (idx-l<0) ? idx-l+n : idx-l;
+			}
+			
+			arr[idx] += 1;
+		}
+		
+		System.out.println(cnt-1);
+	}
+}
+
