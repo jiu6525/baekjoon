a441c2e880e591f30b0bf4773278b0bfb5c07d9c
jiu <118248199+jiu6525@users.noreply.github.com>
2024-01-25 09:16:46 +0900

    [Silver IV] Title: ATM, Time: 132 ms, Memory: 14484 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -1,36 +1,30 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.Arrays;
 import java.util.StringTokenizer;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+
+
 
 public class Main {
     public static void main(String[] args) throws IOException {
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     	StringTokenizer st;
     	int n = Integer.parseInt(br.readLine());
-    	
-    	List<Integer> arr = new ArrayList<>();
+    	int[] arr = new int[n];
+    	int[] result = new int[n+1];
     	st = new StringTokenizer(br.readLine());
     	for(int i = 0; i<n; i++) {
-    		arr.add(Integer.parseInt(st.nextToken()));
+    		arr[i] = Integer.parseInt(st.nextToken());
     	}
     	
-    	Collections.sort(arr);
+    	Arrays.sort(arr);
     	
-    	List<Integer> lst = IntStream.rangeClosed(0, n)
-    			.boxed()
-    			.collect(Collectors.toList());
+    	for(int i = 0; i<n; i++) {
+    		result[i+1] = arr[i] + result[i]; 
+    	}
     	
-        for (int i = 0; i < n; i++) {
-            lst.set(i + 1, arr.get(i) + lst.get(i));
-        }
-    	
-        int sum = lst.stream().mapToInt(Integer::intValue).sum();
+    	int sum = Arrays.stream(result).sum();
     	System.out.println(sum);
    	
     }
