51c29bba0c950161a592b2229cff1ec5b6292fca
jiu <118248199+jiu6525@users.noreply.github.com>
2024-01-25 13:13:20 +0900

    [Bronze II] Title: 블랙잭, Time: 132 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -1,10 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 import java.util.StringTokenizer;
 
 
@@ -13,49 +10,30 @@ import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        st = new StringTokenizer(br.readLine());
-        int[] arr = new int[n];
-        for(int i = 0; i<n; i++) {
-        	arr[i] = Integer.parseInt(st.nextToken());
-        }
-        
-        List<List<Integer>> combinations = combination(arr, 3);
-        List<Integer> ansLst = new ArrayList<>(); 
-        
-        for (List<Integer> combination : combinations) {
-        	int sum = combination.stream().mapToInt(Integer::intValue).sum();
-        	ansLst.add(sum);
-        }
-        Collections.sort(ansLst, Collections.reverseOrder());
-        
-        for (Integer ans : ansLst) {
-			if(ans<=m) {
-				System.out.println(ans);
-				break;
-			}
-		}
-    }
+    	 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+         StringTokenizer st = new StringTokenizer(br.readLine());
+         int n = Integer.parseInt(st.nextToken());
+         int m = Integer.parseInt(st.nextToken());
+         int ans = 0;
+         
+         st = new StringTokenizer(br.readLine());
+         int[] arr = new int[n];
+         for(int i = 0; i<n; i++) {
+         	arr[i] = Integer.parseInt(st.nextToken());
+         }
 
-    private static List<List<Integer>> combination(int[] arr, int r) {
-        List<List<Integer>> result = new ArrayList<>();
-        combinationHelper(arr, 0, new ArrayList<>(), r, result);
-        return result;
-    }
-
-    private static void combinationHelper(int[] arr, int start, List<Integer> current, int r, List<List<Integer>> result) {
-        if (r == 0) {
-            result.add(new ArrayList<>(current));
-            return;
-        }
-
-        for (int i = start; i < arr.length; i++) {
-            current.add(arr[i]);
-            combinationHelper(arr, i + 1, current, r - 1, result);
-            current.remove(current.size() - 1);
-        }
+         for(int i = 0; i<n; i++) {
+        	 int num = 0;
+        	 for(int j = i+1; j<n; j++) {
+        		 for(int k = j+1; k<n; k++) {
+        			 num = arr[i]+arr[j]+arr[k];
+        			 if(num<=m) {
+        				 ans = Math.max(ans, num);
+        			 }
+        		 }
+        	 }
+         }
+         System.out.println(ans);
+   	
     }
 }
