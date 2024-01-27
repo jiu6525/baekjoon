45882efb4fa5acec4303d372dd562882fc5b6603
jiu <118248199+jiu6525@users.noreply.github.com>
2024-01-27 20:37:04 +0900

    [D3] Title: 재미있는 오셀로 게임, Time: 164 ms, Memory: 22,156 KB -BaekjoonHub

--- SWEA/D3/4615. 재미있는 오셀로 게임/재미있는 오셀로 게임.java
+++ SWEA/D3/4615. 재미있는 오셀로 게임/재미있는 오셀로 게임.java
@@ -1,15 +1,30 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.Stack;
 import java.util.StringTokenizer;
+
+//1
+//4 12
+//1 2 1
+//1 1 2
+//4 3 1
+//4 4 2
+//2 1 1
+//4 2 2
+//3 4 1
+//1 3 2
+//2 4 1
+//1 4 2
+//4 1 2
+//3 1 2
 
 public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
-
-        for (int start = 1; start <= t; start++) {
+        for(int start = 1; start<=t; start++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int N = Integer.parseInt(st.nextToken());
             int M = Integer.parseInt(st.nextToken());
@@ -26,35 +41,35 @@ public class Solution {
                 int sj = Integer.parseInt(st.nextToken());
                 int d = Integer.parseInt(st.nextToken());
                 arr[si][sj] = d;
-
-                for (int[] direction : q) {
+                for (int j = 0; j < q.length; j++) {
                     Stack<Integer> r = new Stack<>();
-                    int ni = si + direction[0];
-                    int nj = sj + direction[1];
-
-                    while (1 <= ni && ni <= N && 1 <= nj && nj <= N) {
-                        if (arr[ni][nj] == 0) {
-                            break;
-                        } else if (arr[ni][nj] == d) {
-                            while (!r.isEmpty()) {
-                                int tj = r.pop();
-                                int ti = r.pop();
-                                arr[ti][tj] = d;
+                    for (int mul = 1; mul < N; mul++) {
+                        int ni = si + q[j][0] * mul;
+                        int nj = sj + q[j][1] * mul;
+                        if (1 <= ni && ni <= N && 1 <= nj && nj <= N) {
+                            if (arr[ni][nj] == 0) {
+                                break;
+                            } else if (arr[ni][nj] == d) {
+                                while (r.size() > 0) {
+                                    int tj = r.pop();
+                                    int ti = r.pop();
+                                    arr[ti][tj] = d;
+                                }
+                                break;
+                            } else {
+                                r.push(ni);
+                                r.push(nj);
                             }
-                            break;
                         } else {
-                            r.push(ni);
-                            r.push(nj);
-                            ni += direction[0];
-                            nj += direction[1];
+                            break;
                         }
                     }
                 }
             }
 
             int bcnt = 0, wcnt = 0;
-            for (int[] row : arr) {
-                for (int value : row) {
+            for (int[] lst : arr) {
+                for (int value : lst) {
                     if (value == 1) {
                         bcnt++;
                     } else if (value == 2) {
@@ -62,8 +77,22 @@ public class Solution {
                     }
                 }
             }
-
-            System.out.printf("#%d %d %d%n", start, bcnt, wcnt);
+            System.out.printf("#%d %d %d\n",start,bcnt,wcnt);
         }
     }
 }
+
+
+
+
+//게임을 시작하며 N+1 짜리 이차원 배열을 선언
+//        배열의 (m,m), (m+1,m+1) 위치에는 2(백돌)을
+//        배열의 (m+1,m), (m,m+1) 위치에는 1(흑돌)로 초기화
+//        start 메서드
+//        M개의 입력을 받으며 해당 위치에 돌 놓기
+//        주어진 범위를 탐색하며 돌을 저장할 스택 r 선언
+//        팔방탐색을 통해 mul(1,N) 까지의 범위만큼 값 확인
+//        만약 ni,nj가 0일경우 break
+//        ni,nj가 d의 값과 일치한다면 스택이 빌때까지 스택에서 값을 pop한 뒤
+//        전부 d로 변경해줌
+//        0도 아니고 d 도 아니라면 값을 스택의 누적
