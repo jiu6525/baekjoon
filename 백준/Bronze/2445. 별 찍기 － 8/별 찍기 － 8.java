1488a8c9d6788c4a3a1462f860e70215504e0fd0
jiu <118248199+jiu6525@users.noreply.github.com>
2024-01-28 20:31:34 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 556 ms, Memory: 21440 KB -BaekjoonHub

---
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+//        int n = 5;
+        int cnt = 1;
+
+        for(int i = 0; i<2*n-1; i++){
+            for(int j = 0; j<2*n; j++){
+                if(j<cnt || j>=2*n-cnt){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+            if(i<n-1){
+                cnt++;
+            }else{
+                cnt--;
+            }
+        }
+    }
+}
+
