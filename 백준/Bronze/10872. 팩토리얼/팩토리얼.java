aa901f86c0a5a9cf9853848806fe30d3e9a763e5
jiu <118248199+jiu6525@users.noreply.github.com>
2024-01-29 21:54:53 +0900

    [Bronze V] Title: 팩토리얼, Time: 120 ms, Memory: 14304 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int fact = fact(n);
+        System.out.println(fact);
+    }
+
+    private static int fact(int n) {
+        if(n <= 1){
+            return 1;
+        }
+        return n * fact(n-1);
+    }
+}
+
