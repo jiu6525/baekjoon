73da51327a054953501b9c16d2eede931bdc99bd
jiu <118248199+jiu6525@users.noreply.github.com>
2024-01-31 21:43:16 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1708 ms, Memory: 127460 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -17,17 +17,18 @@ public class Main {
 //        int[][] arr = {{1,2,3,4},{2,3,4,5},{3,4,5,6},{4,5,6,7}};
         int[][] dp = new int[n+1][n+1];
 
-        for(int i = 0;i<n;i++){
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < n; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
+//        for(int i = 0;i<n;i++){
+//            st = new StringTokenizer(br.readLine());
+//            for (int j = 0; j < n; j++) {
+//                arr[i][j] = ;
+//            }
+//        }
 
-        dp[1][1] = arr[0][0];
+//        dp[1][1] = arr[0][0];
         for (int i = 1; i < dp.length; i++) {
+            st = new StringTokenizer(br.readLine());
             for (int j = 1; j < dp.length; j++) {
-                dp[i][j] = dp[i-1][j] + dp[i][j-1] + arr[i-1][j-1] - dp[i-1][j-1];
+                dp[i][j] = dp[i-1][j] + dp[i][j-1] + Integer.parseInt(st.nextToken()) - dp[i-1][j-1];
 //                System.out.print(dp[i][j] + " ");
             }
 //            System.out.println();
