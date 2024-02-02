2817ca10bd366cd8d4b9c02ab7030cc38ea0510f
jiu <118248199+jiu6525@users.noreply.github.com>
2024-02-02 14:23:29 +0900

    [Gold V] Title: 신기한 소수, Time: 120 ms, Memory: 14152 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.java
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -1,47 +1,47 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.List;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringBuilder sb = new StringBuilder();
-
-	public static void main(String[] args) throws IOException {
-		int n = Integer.parseInt(br.readLine());
-		getSolv(0,n);
-
-//		System.out.println(sb.substring(0,sb.length()));
-		System.out.println(sb);
-	}
-
-	private static void getSolv(int t, int n) {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static int ans;
+	static int N;
+    public static void main(String[] args) throws IOException {
+    	N = Integer.parseInt(br.readLine());
+    	
+    	recursive(0,N);
+    }
+	private static void recursive(int val, int n) {
+		// basis part
 		if(n==0) {
-			if (isPrime(t)) {
-				sb.append(t).append("\n");
-			}
+			System.out.println(val);
+			return;
 		}
-		for(int i = 0; i<10; i++) {
-			int num = t * 10 + i;
-//			System.out.println(num);
+		
+		// inductive part
+		for (int i = 1; i < 10; i++) {
+			int num = val*10+i;
+			// 만약에 num 이 소수라면 num 의 값을 확정하고
 			if(isPrime(num)) {
-				getSolv(num,n-1);
+				recursive(num, n-1);
 			}
 		}
+		
 	}
-	
-	private static boolean isPrime(int n) {
-		if(n<2) {
+	private static boolean isPrime(int num) {
+		if(num<2) {
 			return false;
 		}
 		
-		for(int i = 2; i<=Math.sqrt(n); i++) {
-			if(n%i==0) {
+		for(int i = 2; i<=Math.sqrt(num);i++) {
+			if(num%i==0) {
 				return false;
 			}
 		}
-		
 		return true;
 	}
 }
