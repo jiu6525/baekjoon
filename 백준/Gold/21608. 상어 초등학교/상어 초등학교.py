69af25923c74434a032d4b7497cc819da4d93171
jiu <118248199+jiu6525@users.noreply.github.com>
2024-02-04 20:55:41 +0900

    [Gold V] Title: 상어 초등학교, Time: 272 ms, Memory: 116924 KB -BaekjoonHub

--- 백준/Gold/21608. 상어 초등학교/상어 초등학교.py
+++ 백준/Gold/21608. 상어 초등학교/상어 초등학교.py
@@ -8,12 +8,10 @@ for i in range(n**2):
 ans = 0
 for i in range(1,len(d)):
     lst = d[i][1:]
-    ck_lst,zck_lst = [],[]
+    ck_lst = []
     for i0 in range(1,len(arr)):
         for i1 in range(1,len(arr)):
             ck,zck = 0,0
-            # print(i0,i1)
-            # print(arr[1][2])
             if not arr[i0][i1]:
                 for x,y in((-1,0),(1,0),(0,-1),(0,1)):
                     dx = x + i0
@@ -25,29 +23,11 @@ for i in range(1,len(d)):
                             ck += 1
                         elif not arr[dx][dy]:
                             zck += 1
-                if ck:
-                    ck_lst.append([i0,i1,ck,zck])
-                else:
-                    zck_lst.append([i0,i1,zck])
+                ck_lst.append([i0,i1,ck,zck])
+
                 
     ck_lst.sort(key=lambda x: (-x[2],-x[3],x[0],x[1]))
-    zck_lst.sort(key=lambda x: (-x[2]))
-    if not ck_lst:
-        for zc in zck_lst:
-            zci,zcj = zc[0],zc[1]
-            if arr[zci][zcj]:
-                continue
-            else:
-                arr[zci][zcj] = d[i][0]
-                break
-    else:
-        for c in ck_lst:
-            ci,cj = c[0],c[1]
-            if arr[ci][cj]:
-                continue
-            else:
-                arr[ci][cj] = d[i][0]
-                break
+    arr[ck_lst[0][0]][ck_lst[0][1]] = d[i][0]
 
 d.sort(key=lambda x:x[0])
 
