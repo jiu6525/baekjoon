c9deb94ac3604ab1c5a2211d60b4d8bddc14b3ef
jiu <118248199+jiu6525@users.noreply.github.com>
2024-02-15 13:25:48 +0900

    [Gold II] Title: 빵집, Time: 396 ms, Memory: 34560 KB -BaekjoonHub

--- 백준/Gold II/3109. 빵집/빵집.java
+++ 백준/Gold II/3109. 빵집/빵집.java
@@ -1,17 +1,22 @@
+//package jiu;
+
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedReader br;
     static StringTokenizer st;
     static int n, m, ans;
     static char[][] arr;
-    static boolean flag = false;
+//    static boolean flag = false;
     static int[] dir = {-1, 0, 1};  // 오른쪽 위 대각선, 오른쪽, 오른쪽 아래 대각선
 
     public static void main(String[] args) throws IOException {
+    	//System.setIn(new FileInputStream("src/jiu/Main.txt"));
+    	br = new BufferedReader(new InputStreamReader(System.in));
         st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
@@ -25,19 +30,16 @@ public class Main {
         }
 
         for (int i = 0; i < n; i++) {
-            dfs(i, 0);
-            if(flag){
-                ans++;
-                flag = false;
+            if(dfs(i, 0)) {
+            	ans++;
             }
         }
         System.out.println(ans);
     }
 
-    public static void dfs(int x, int y) {
+    public static boolean dfs(int x, int y) {
         if (y == m - 1) {
-            flag = true;
-            return;
+            return true;
         }
 
         for (int i = 0; i < dir.length; i++) {
@@ -50,10 +52,10 @@ public class Main {
                 continue;
             }
             arr[nx][ny] = 'x';
-            dfs(nx, ny);
-            if (flag) {
-                return;
+            if(dfs(nx, ny)) {
+            	return true;
             }
         }
+		return false;
     }
 }
