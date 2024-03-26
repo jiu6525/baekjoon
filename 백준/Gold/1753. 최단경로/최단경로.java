2f59830e9ecacab3ccac6c78c755f25bed79f157
jiu <118248199+jiu6525@users.noreply.github.com>
2024-03-26 22:05:34 +0900

    [Gold IV] Title: 최단경로, Time: 1080 ms, Memory: 110480 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,98 +1,107 @@
-//package exam.month2.exam0226;
+import java.io.*;
+import java.util.*;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
-
-// 인접리스트 버전
-// 1753 최단경로
 public class Main {
-	static class Node{
-		int vertex,weight;
-		Node next;
-		
-		public Node(int vertex, int weight, Node next) {
-			super();
-			this.vertex = vertex;
-			this.weight = weight;
-			this.next = next;	
-		}
-		@Override
-		public String toString() {
-			return "Node [vertex=" + vertex + ", weight=" + weight + ", link=" + next + "]";
-		}
-	}
-	public static void main(String[] args) throws IOException {
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(in.readLine().trim());
-		int V = Integer.parseInt(st.nextToken()); //정점 갯수
-		int E = Integer.parseInt(st.nextToken()); //간선 갯수
-		st = new StringTokenizer(in.readLine().trim());
-		int start = Integer.parseInt(st.nextToken());//시작점 인덱스
-//		int end =  Integer.parseInt(st.nextToken()); //도착점 인덱스
-		final int INF = Integer.MAX_VALUE;
-		
-		Node[] adjList = new Node[V+1];
-		int[] minDinstance = new int[V+1];
-		boolean[] visited = new boolean[V+1];
-		
-		for (int i = 0; i < E; i++) {
-			st = new StringTokenizer(in.readLine().trim(), " ");
-			int from = Integer.parseInt(st.nextToken());
-			int to = Integer.parseInt(st.nextToken());
-			int weight = Integer.parseInt(st.nextToken());
-			adjList[from] = new Node(to,weight,adjList[from]);
-		} // 인접리스트 생성
-		
-//		print(adjList);
-		Arrays.fill(minDinstance, INF);
-		minDinstance[start] = 0; // 출발지에서 출발지로의비용 0으로 초기화
-		
-		int min = 0 , stopOver = 0;
-		for (int i = 1; i <= V; i++) { //모든 정점이 다 처리될때까지 반복
-			
-			//step1 : 미방문 정점 중 출발지에서 가장 가까운 정점 선택
-			min = INF;
-			stopOver = -1;
-			
-			for (int j = 1; j <= V; j++) {
-				if(!visited[j] && min > minDinstance[j]) {
-					min = minDinstance[j];
-					stopOver = j;
-				}
-			}
-			
-			if(stopOver == -1) break;
-			visited[stopOver] = true;
-//			if(stopOver == end) break;  // 도착점이면 끝내기!!
-			
-			// step2 : 미방문 정점들에 대해 선택된 경유지를 거쳐서 가는 비용과 기존 최소비용을 비교해서 업데이트
-			for (Node temp = adjList[stopOver]; temp != null; temp = temp.next) {
-				if(//!visited[temp.vertex] && 
-						minDinstance[temp.vertex] > min + temp.weight) {
-					minDinstance[temp.vertex] = min + temp.weight; 
-				}
-			}
-		}
-		
-		for (int i = 1; i < minDinstance.length; i++) {
-			int ans = minDinstance[i];
-			System.out.println(ans == INF ? "INF" : ans);
-		}
-		
-//		System.out.println(Arrays.toString(minDinstance));
-		
+    static class Node implements Comparable<Node>{
+        int from,to,weight;
 
-	}
-	private static void print(Node[] adjList) {
-		for (Node node : adjList) {
-			System.out.println(node);
-		}
-		
-	}
+        public Node(int from, int to, int weight) {
+            this.from = from;
+            this.to = to;
+            this.weight = weight;
+        }
 
+        public Node(int to, int weight) {
+            this.to = to;
+            this.weight = weight;
+        }
+
+        @Override
+        public String toString() {
+            return "Node [from=" + from + ", to=" + to + ", weight=" + weight + "]";
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return Integer.compare(this.weight, o.weight);
+        }
+
+    }
+
+    static BufferedReader br;
+    static StringTokenizer st;
+    static int V,E;
+    static int k;
+    static List<List<Node>> lst;
+    static final int INF = Integer.MAX_VALUE;
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
+        V = Integer.parseInt(st.nextToken());
+        E = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(br.readLine());
+        lst = new ArrayList<>();
+
+        for (int i = 0; i < V+1; i++) {
+            lst.add(i, new ArrayList<>());
+        }
+
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+
+            lst.get(from).add(new Node(from, to, weight));
+        }
+
+//        print();
+        prim();
+    }
+    private static void prim() {
+        boolean[] v = new boolean[V+1];
+        int[] minEdge = new int[V+1];
+
+        Arrays.fill(minEdge, INF);
+        minEdge[k] = 0;
+        PriorityQueue<Node> q = new PriorityQueue<>();
+        q.offer(new Node(k, minEdge[k]));
+
+        int c = 0;
+        int min = 0;
+        while(!q.isEmpty()) {
+            Node minVertex = q.poll();
+
+            if(v[minVertex.to]) {
+                continue;
+            }
+
+            min = minVertex.weight;
+            v[minVertex.to] = true;
+
+            if(++c == V) {
+                break;
+            }
+
+            List<Node> nodes = lst.get(minVertex.to);
+            for (int i = 0; i < nodes.size(); i++) {
+                if(!v[nodes.get(i).to] && min+nodes.get(i).weight < minEdge[nodes.get(i).to]) {
+                    minEdge[nodes.get(i).to] = min+nodes.get(i).weight;
+                    q.offer(new Node(nodes.get(i).to,minEdge[nodes.get(i).to]));
+                }
+            }
+
+
+        }
+
+        for (int i = 1; i < minEdge.length; i++) {
+            System.out.println(minEdge[i] == INF? "INF" : minEdge[i]);
+        }
+
+    }
+    private static void print() {
+        for (List<Node> list : lst) {
+            System.out.println(list);
+        }
+    }
 }
-
-
