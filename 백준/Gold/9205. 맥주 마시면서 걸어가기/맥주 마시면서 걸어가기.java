81f1fb6633e7c56fa32a5feb9f2456da9bbca12c
jiu <118248199+jiu6525@users.noreply.github.com>
2024-03-28 15:08:03 +0900

    [Gold V] Title: 맥주 마시면서 걸어가기, Time: 168 ms, Memory: 15068 KB -BaekjoonHub

---
+++ 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	static int T, N;
+	static List<int[]> lst;
+	public static void main(String[] args) throws Exception {
+    	//System.setIn(new FileInputStream("src/jiu/Main.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		T = Integer.parseInt(br.readLine());
+		for(int tc=1; tc<=T; tc++) {
+			N = Integer.parseInt(br.readLine());
+			st = new StringTokenizer(br.readLine());
+			int sj = Integer.parseInt(st.nextToken());
+			int si = Integer.parseInt(st.nextToken());
+			
+			lst = new ArrayList<>();
+			for (int i = 0; i < N; i++) {
+				st = new StringTokenizer(br.readLine());
+				int tj = Integer.parseInt(st.nextToken());
+				int ti = Integer.parseInt(st.nextToken());
+				lst.add(new int[] {tj,ti});
+			}
+			st = new StringTokenizer(br.readLine());
+			int ej = Integer.parseInt(st.nextToken());
+			int ei = Integer.parseInt(st.nextToken());
+			
+			String ans = bfs(sj,si,ej,ei);
+			System.out.println(ans);
+		}
+	}
+
+	private static String bfs(int sj, int si, int ej, int ei) {
+		Queue<int[]> q = new LinkedList<>();
+		boolean[] v = new boolean[N];
+		
+		q.offer(new int[] {sj,si});
+		while(!q.isEmpty()) {
+			int[] tmp = q.poll();
+			int cj = tmp[0];
+			int ci = tmp[1];
+			if(Math.abs(cj-ej) + Math.abs(ci-ei) <= 1000) {
+				return "happy";
+			}
+			for (int i = 0; i < N; i++) {
+				if(!v[i]) {
+					int[] l = lst.get(i);
+					int nj = l[0];
+					int ni = l[1];
+					if(Math.abs(cj-nj) + Math.abs(ci-ni) <= 1000) {
+						q.offer(new int[] {nj,ni});
+						v[i] = true;
+					}
+					
+				}
+			}
+			
+			
+		}
+		return "sad";
+	}
+
+	
+}
