629c0c3d7f28be193feb50473ea040e6612aa3e7
jiu <118248199+jiu6525@users.noreply.github.com>
2024-03-29 10:51:15 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 13.19 ms, Memory: 82.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -0,0 +1,69 @@
+import java.util.*;
+class Solution {
+    public int solution(int n, int[][] wires) {
+		List<List<Integer>> arr = new ArrayList<>();
+		int size = findMax(wires)+1;
+		int ans = Integer.MAX_VALUE;
+		
+		for (int i = 0; i < wires.length; i++) {
+			arr = idj(arr, size);
+			for (int j = 0; j < wires.length; j++) {
+				if(i==j) {
+					continue;
+				}else {
+					int[] l = wires[j];
+					arr.get(l[0]).add(l[1]);
+					arr.get(l[1]).add(l[0]);
+				}
+			}
+			Set<Integer> set = new HashSet<>();
+			
+			for (int k = 1; k < size; k++) {
+				if(set.size() == 2) {
+			        Integer[] diff = set.toArray(new Integer[0]);
+			        ans = Math.min(ans, Math.abs(diff[0] - diff[1]));
+					break;
+				}
+				int dfs = dfs(arr,k,new boolean[arr.size()]);
+				set.add(dfs);
+			}
+            if(set.size() == 1) {
+				ans = 0;
+			}
+		}
+		System.out.println(ans);
+        return ans;
+	}
+
+
+	private static List<List<Integer>> idj(List<List<Integer>> arr, int size) {
+		arr.clear();
+		for (int i = 0; i < size; i++) {
+			arr.add(i,new ArrayList<>());
+		}
+		return arr;
+	}
+	
+	
+	private static int dfs(List<List<Integer>> graph, int v, boolean[] visited) {
+		visited[v] = true;
+		int cnt = 1;
+		for (Integer i : graph.get(v)) {
+			if(!visited[i]) {
+				cnt += dfs(graph,i,visited);
+			}
+		}
+		
+		return cnt;
+		
+	}
+	private static int findMax(int[][] lst) {
+		int ans = Integer.MIN_VALUE;
+		for (int[] l : lst) {
+			for (int i = 0; i < l.length; i++) {
+				ans = Math.max(ans, l[i]);
+			}
+		}
+		return ans;
+	}
+}
