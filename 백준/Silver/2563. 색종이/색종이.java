d9a60af53a5da3060458fa0bb24053db1c35f059
jiu <118248199+jiu6525@users.noreply.github.com>
2024-06-20 18:00:30 +0900

    [Silver V] Title: 색종이, Time: 104 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -1,67 +1,37 @@
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
 
-public class Main {
-	static BufferedReader br;
-	static int n,ans;
-	static StringTokenizer st;
-	static int[][] board = new int[100][100];
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		//System.setIn(new FileInputStream("src/jiu/Main.txt"));
-		br = new BufferedReader(new InputStreamReader(System.in));
-		n = Integer.parseInt(br.readLine());
-		
-		for(int i = 0; i<n; i++) {
-			st = new StringTokenizer(br.readLine());
-			int x = Integer.parseInt(st.nextToken());
-			int y = Integer.parseInt(st.nextToken());
-			rc(x,y,0);
-		}
-		
-		System.out.println(ans);
-		
-		
-	}
-	private static void rc(int i, int j, int k) {
-	    // 기저 조건
-	    if (i >= 100 || j >= 100 || k > 9) {
-	        return;
-	    }
+class Main {
+    static int n,m;
+    static int[][] arr = new int[100][100];
+    static int s, e, ans;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+    static Stack<Integer> stack = new Stack<>();
+    public static void main(String[] args) throws IOException {
+        n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int w = Integer.parseInt(st.nextToken())-1;
+            int h = Integer.parseInt(st.nextToken())-1;
+            for (int j = w; j < w+10; j++) {
+                for (int k = h; k < h+10; k++) {
+                    arr[j][k] = 1;
+                }
+            }
+        }
 
-	    // 배열의 값이 0일 때만 채우고, 값이 1로 채워지면 ans 증가
-	    if (board[i][j] == 0) {
-	        board[i][j] = 1;
-	        ans++;
-	    }
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 100; j++) {
+                if(arr[i][j] == 1){
+                    ans++;
+                }
+            }
+        }
 
-	    // 재귀 호출
-	    rc(i, j + 1, k + 1);
-	    dc(i,j,0);
-	}
-	private static void dc(int i, int j, int k) {
-	    if (i >= 100 || j >= 100 || k > 9) {
-	        return;
-	    }
-
-	    // 배열의 값이 0일 때만 채우고, 값이 1로 채워지면 ans 증가
-	    if (board[i][j] == 0) {
-	        board[i][j] = 1;
-	        ans++;
-	    }
-		
-	    dc(i+1,j,k+1);
-	}
-
+        System.out.println(ans);
+    }
 }
-
-
-
-
-//3
-//3 7
-//15 7
-//5 2
