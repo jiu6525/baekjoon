6508ded6a9cf83a9e5a931e5ca5a2a7bdb97517e
jiu <118248199+jiu6525@users.noreply.github.com>
2024-08-19 14:13:41 +0900

    [Silver III] Title: 계단 오르기, Time: 100 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -1,35 +1,43 @@
 import java.io.*;
-import java.util.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 public class Main {
-	static BufferedReader br;
-	static StringTokenizer st;
-	static int n;
-	public static void main(String[] args) throws Exception {
-//    	System.setIn(new FileInputStream("src/jiu/main.txt"));
-    	br = new BufferedReader(new InputStreamReader(System.in));
-    	n = Integer.parseInt(br.readLine());
-    	
-    	int[] arr = new int[n+1];
-    	
-//    	0 밟지 않은경우
-//    	1 한번 밟
-//    	2 연속
-    	
-    	
-    	int[][] dp = new int[n+1][3];
-    	for (int i = 1; i <= n; i++) {
-			arr[i] = Integer.parseInt(br.readLine());
-		}
-    	
-        
-		for(int i =1; i <=n; i++) {
-			dp[i][0] = Math.max(dp[i-1][1], dp[i-1][2]);
-			dp[i][1] = dp[i-1][0] + arr[i];
-			dp[i][2] = dp[i-1][1] + arr[i];
-		}
-        
-//        System.out.println(Arrays.toString(dp));
-		System.out.println(Math.max(dp[n][1], dp[n][2]));
-        
+
+    public static BufferedReader br;
+    public static StringTokenizer st;
+    public static int n;
+    public static int[] arr;
+    public static int[] dp;
+    public static void main(String[] args) throws IOException {
+        //System.setIn(new FileInputStream("src/input.txt"));
+        br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n+1];
+        dp = new int[n+1];
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+//        System.out.println(Arrays.toString(arr));
+
+//        d(n-3) + f(n) + f(n-1)
+//        d(n-2) + f(n)
+        if(n == 1){
+            dp[1] = arr[1];
+        }else if(n == 2){
+            dp[1] = arr[1];
+            dp[2] = arr[1] + arr[2];
+        } else{
+            dp[1] = arr[1];
+            dp[2] = arr[1] + arr[2];
+            dp[3] = Math.max(arr[1] + arr[3], arr[2] + arr[3]);
+        }
+
+        for (int i = 4; i <= n; i++) {
+            dp[i] = Math.max((dp[i-3] + arr[i] + arr[i-1]), dp[i-2] + arr[i]);
+        }
+
+        System.out.println(dp[n]);
     }
 }
