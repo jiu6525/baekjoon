5b0ab543aca4bd18d8746e441ab4924641ba50f1
jiu <118248199+jiu6525@users.noreply.github.com>
2024-11-03 18:37:23 +0900

    [Bronze II] Title: Hashing, Time: 108 ms, Memory: 14336 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -2,22 +2,24 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 import java.util.*;
 
     public class Main {
         static BufferedReader br;
         static StringTokenizer st;
         static int n;
-        static int ans;
+        static BigInteger ans = new BigInteger("0");
         public static void main(String[] args) throws IOException {
             //System.setIn(new FileInputStream("src/input.txt"));
             br = new BufferedReader(new InputStreamReader(System.in));
             n = Integer.parseInt(br.readLine());
             String s = br.readLine();
-            for (int i = 0; i < s.length(); i++) {
-                ans += (int) ((s.charAt(i) - 96) * Math.pow(31,i));
+            int m = 1234567891;
+            for (int i = 0; i < n; i++) {
+                ans = ans.add(BigInteger.valueOf(s.charAt(i) - 96).multiply(BigInteger.valueOf(31).pow(i)));
             }
 
-            System.out.println(ans);
+            System.out.println(ans.remainder(BigInteger.valueOf(m)));
         }
     }
